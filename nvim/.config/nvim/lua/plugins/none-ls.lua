17bdc361f272e9c06bf872ff956c2667bbf190dd
RaviParvadiya <raviparvadiya.2001@gmail.com>
2025-06-22 16:06:18 +0530

    fixing config changes

--- nvim/.config/nvim/lua/plugins/none-ls.lua
+++ nvim/.config/nvim/lua/plugins/none-ls.lua
@@ -1,3 +1,41 @@
+return {
+    "nvimtools/none-ls.nvim",
+    dependencies = {
+      "nvimtools/none-ls-extras.nvim",
+    },
+    config = function()
+        local null_ls = require("null-ls")
+        null_ls.setup({
+            sources = { 
+                -- PHP: Linting and formatting
+                null_ls.builtins.diagnostics.phpcs.with({
+                    extra_args = {"--standard=PSR12"}
+                }),
+                null_ls.builtins.formatting.phpcbf.with({
+                    extra_args = {"--standard=PSR12"}
+                }),
+
+                -- JS/TS/HTML/CSS/JSON/SCSS: Prettier
+                null_ls.builtins.formatting.prettier,
+
+                -- Python: Black for formatting, flake8 for linting
+                null_ls.builtins.formatting.black,
+                -- null_ls.builtins.diagnostics.flake8, -- Use extras!
+
+                -- Go: gofumpt for formatting, golangci-lint for linting
+                null_ls.builtins.formatting.gofumpt,
+                null_ls.builtins.diagnostics.golangci_lint,
+
+                -- Shell: shellcheck for linting, shfmt for formatting
+                -- null_ls.builtins.diagnostics.shellcheck, -- Use extras!
+                null_ls.builtins.formatting.shfmt,
+            }
+        })
+    end
+}
+
+
+
 -- return {
 --   "nvimtools/none-ls.nvim",
 --   config = function()
@@ -14,26 +52,3 @@
 --     vim.keymap.set("n", "<leader>gf", vim.lsp.buf.format, {})
 --   end,
 -- }
-return {
-    "nvimtools/none-ls.nvim",
-    config = function()
-        local null_ls = require("null-ls")
-        null_ls.setup({
-            sources = { -- PHP: Linting and formatting
-            null_ls.builtins.diagnostics.phpcs.with({
-                extra_args = {"--standard=PSR12"}
-            }), null_ls.builtins.formatting.phpcbf.with({
-                extra_args = {"--standard=PSR12"}
-            }), -- JS/TS/HTML/CSS/JSON/SCSS: Prettier
-            null_ls.builtins.formatting.prettier, -- Python: Black for formatting, flake8 for linting
-            null_ls.builtins.formatting.black, null_ls.builtins.diagnostics.flake8,
-
-            -- Go: gofumpt for formatting, golangci-lint for linting
-            null_ls.builtins.formatting.gofumpt, null_ls.builtins.diagnostics.golangci_lint,
-
-            -- Shell: shellcheck for linting, shfmt for formatting
-            null_ls.builtins.diagnostics.shellcheck, null_ls.builtins.formatting.shfmt},
-           
-        })
-    end
-}
